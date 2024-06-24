94e15197e3875f96c77f3ce78da3e7f370973c29
Ilya <ib258650@gmail.com>
2024-06-24 16:44:23 +0500

    Standar

---
+++ Profile/Profile_me/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(AboutMe)
+admin.site.register(ProgrammingLanguage)
+admin.site.register(Project)
+admin.site.register(WordsForEmployer)
+
